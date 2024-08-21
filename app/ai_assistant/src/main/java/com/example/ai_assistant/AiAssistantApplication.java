31dd786d41e9c5d81a6c6b43e02159349fedcdd9
yujiangjun <18512106361@163.com>
2024-08-21 19:13:39 +0800

    app init

---
+++ app/ai_assistant/src/main/java/com/example/ai_assistant/AiAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAssistantApplication.class, args);
+	}
+
+}
