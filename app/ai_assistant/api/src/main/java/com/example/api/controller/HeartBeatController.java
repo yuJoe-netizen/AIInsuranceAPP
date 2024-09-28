2d244c3f4872674f4bb4bf839c84f521b5c82f90
OrionTate <18512106361@163.com>
2024-09-28 20:36:03 +0800

    部署

--- app/ai_assistant/api/src/main/java/com/example/api/controller/HeartBeatController.java
+++ app/ai_assistant/api/src/main/java/com/example/api/controller/HeartBeatController.java
@@ -1,19 +1,19 @@
-package com.example.api.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HeartBeatController {
-    
-
-    @Autowired
-    private ServerProperties serverProperties;
-
-    @GetMapping
-    public int getPort(){
-        return serverProperties.getPort();
-    }
-}
+//package com.example.api.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.web.ServerProperties;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HeartBeatController {
+//
+//
+//    @Autowired
+//    private ServerProperties serverProperties;
+//
+//    @GetMapping
+//    public int getPort(){
+//        return serverProperties.getPort();
+//    }
+//}
--- app/ai_assistant/api/src/main/java/com/example/api/mq/SendMQ.java
+++ app/ai_assistant/api/src/main/java/com/example/api/mq/SendMQ.java
@@ -1,41 +1,41 @@
-package com.example.api.mq;
-
-
-import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.client.apis.ClientException;
-import org.apache.rocketmq.client.apis.ClientServiceProvider;
-import org.apache.rocketmq.client.apis.message.Message;
-import org.apache.rocketmq.client.apis.producer.Producer;
-import org.apache.rocketmq.client.apis.producer.SendReceipt;
-
-import java.nio.charset.StandardCharsets;
-
-@Slf4j
-public class SendMQ {
-
-    public static void main(String[] args) throws ClientException {
-        final ClientServiceProvider provider = ClientServiceProvider.loadService();
-
-        String topic = "AIAgent_demo";
-        final Producer producer = ProducerSingleton.getInstance(topic);
-        // Define your message body.
-        byte[] body = "This is a normal message for Apache RocketMQ".getBytes(StandardCharsets.UTF_8);
-        String tag = "yourMessageTagA";
-        final Message message = provider.newMessageBuilder()
-                // Set topic for the current message.
-                .setTopic(topic)
-                .setMessageGroup("test")
-                // Message secondary classifier of message besides topic.
-                .setTag(tag)
-                // Key(s) of the message, another way to mark message besides message id.
-                .setKeys("yourMessageKey-1c151062f96e")
-                .setBody(body)
-                .build();
-        try {
-            final SendReceipt sendReceipt = producer.send(message);
-            log.info("Send message successfully, messageId={}", sendReceipt.getMessageId());
-        } catch (Throwable t) {
-            log.error("Failed to send message", t);
-        }
-    }
-}
+//package com.example.api.mq;
+//
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.rocketmq.client.apis.ClientException;
+//import org.apache.rocketmq.client.apis.ClientServiceProvider;
+//import org.apache.rocketmq.client.apis.message.Message;
+//import org.apache.rocketmq.client.apis.producer.Producer;
+//import org.apache.rocketmq.client.apis.producer.SendReceipt;
+//
+//import java.nio.charset.StandardCharsets;
+//
+//@Slf4j
+//public class SendMQ {
+//
+//    public static void main(String[] args) throws ClientException {
+//        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+//
+//        String topic = "AIAgent_demo";
+//        final Producer producer = ProducerSingleton.getInstance(topic);
+//        // Define your message body.
+//        byte[] body = "This is a normal message for Apache RocketMQ".getBytes(StandardCharsets.UTF_8);
+//        String tag = "yourMessageTagA";
+//        final Message message = provider.newMessageBuilder()
+//                // Set topic for the current message.
+//                .setTopic(topic)
+//                .setMessageGroup("test")
+//                // Message secondary classifier of message besides topic.
+//                .setTag(tag)
+//                // Key(s) of the message, another way to mark message besides message id.
+//                .setKeys("yourMessageKey-1c151062f96e")
+//                .setBody(body)
+//                .build();
+//        try {
+//            final SendReceipt sendReceipt = producer.send(message);
+//            log.info("Send message successfully, messageId={}", sendReceipt.getMessageId());
+//        } catch (Throwable t) {
+//            log.error("Failed to send message", t);
+//        }
+//    }
+//}
